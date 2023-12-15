655c68a1b4d7317d36ac0dd83ee98cf3b45191d3
albnzhum <albnzhum@gmail.com>
2023-12-15 19:42:19 +0500

    Updated MenuManager

---
+++ Assets/Code/SceneManagement/StartGame.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Railway.Events;
+using UnityEngine;
+
+namespace Railway.SceneManagement
+{
+    public class StartGame : MonoBehaviour
+    {
+        [SerializeField] private GameSceneSO _sceneToLoad;
+        [SerializeField] private bool _showLoadScreen = default;
+
+        [Header("Broadcasting on")] 
+        [SerializeField] private LoadEventChannelSO _loadScene = default;
+
+        [Header("Listening to")] [SerializeField]
+        private VoidEventChannelSO _onNewGameButton = default;
+
+        [SerializeField] private VoidEventChannelSO _onContinueButton = default;
+
+        private bool _hasSaveData;
+
+        private void Start()
+        {
+            _onNewGameButton.OnEventRaised += StartGameInit;
+            //_onContinueButton.OnEventRaised += ContinuePreviousGame;
+        }
+
+        private void OnDestroy()
+        {
+            _onNewGameButton.OnEventRaised -= StartGameInit;
+            //_onContinueButton.OnEventRaised -= ContinuePreviousGame;
+        }
+
+        private void StartGameInit()
+        {
+            _hasSaveData = false;
+            _loadScene.RaiseEvent(_sceneToLoad, _showLoadScreen);
+        }
+
+        /*private void ContinuePreviousGame()
+        {
+            StartCoroutine(LoadSaveGame());
+        }
+
+        private IEnumerator LoadSaveGame()
+        {
+            //yield return StartCoroutine()
+        }*/
+    }
+}
---
+++ Assets/Code/UI/Test/MainMenu/UIMenuManager.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections;
+using Railway.Events;
+using UnityEngine;
+
+namespace Railway.UI
+{
+    public class UIMenuManager : MonoBehaviour
+    {
+        [SerializeField] private UIMainMenu _mainMenu = default;
+        [SerializeField] private UIPopup _popupPanel = default;
+        [SerializeField] private UISettingsController _settingsPanel;
+
+        [Header("Broadcasting on")] [SerializeField]
+        private VoidEventChannelSO _startNewGameEvent;
+
+        [SerializeField] private VoidEventChannelSO _continueGameEvent;
+
+        private bool _hasSaveData;
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(0.4f);
+            SetMenuScreen();
+        }
+
+        private void SetMenuScreen()
+        {
+            //_hasSaveData = _saveSystem.LoadSaveDataFromDisk();
+            _mainMenu.SetMenuScreen(_hasSaveData);
+            _mainMenu.ContinueButtonAction += _continueGameEvent.RaiseEvent;
+            _mainMenu.NewGameButtonAction += ButtonStartNewGameClicked;
+            _mainMenu.SettingsButtonAction += OpenSettingsScreen;
+            _mainMenu.ExitButtonAction += ShowExitConfirmationPopup;
+        }
+
+        private void ButtonStartNewGameClicked()
+        {
+            if (!_hasSaveData)
+            {
+                ConfirmStartNewGame();
+            }
+            else
+            {
+                ShowStartNewGameConfirmationPopup();
+            }
+        }
+
+        private void ConfirmStartNewGame()
+        {
+            _startNewGameEvent.RaiseEvent();
+            Debug.Log("New Game Event");
+        }
+
+        private void ShowStartNewGameConfirmationPopup()
+        {
+            _popupPanel.ConfirmationResponseAction += StartNewGamePopupResponse;
+            _popupPanel.ClosePopupAction += HidePopup;
+            Debug.Log("PopupShowed");
+
+            _popupPanel.gameObject.SetActive(true);
+            _popupPanel.SetPopup(PopupType.NewGame);
+        }
+
+        private void StartNewGamePopupResponse(bool startNewGameConfirmed)
+        {
+            _popupPanel.ConfirmationResponseAction -= StartNewGamePopupResponse;
+            _popupPanel.ClosePopupAction -= HidePopup;
+
+            _popupPanel.gameObject.SetActive(false);
+
+            if (startNewGameConfirmed)
+            {
+                ConfirmStartNewGame();
+            }
+            else
+            {
+                _continueGameEvent.RaiseEvent();
+            }
+
+            _mainMenu.SetMenuScreen(_hasSaveData);
+        }
+
+        private void HidePopup()
+        {
+            _popupPanel.ClosePopupAction -= HidePopup;
+            _popupPanel.gameObject.SetActive(false);
+            _mainMenu.SetMenuScreen(_hasSaveData);
+        }
+
+        public void OpenSettingsScreen()
+        {
+            _settingsPanel.gameObject.SetActive(true);
+            _settingsPanel.Closed += CloseSettingsScreen;
+        }
+
+        public void CloseSettingsScreen()
+        {
+            _settingsPanel.Closed -= CloseSettingsScreen;
+            _settingsPanel.gameObject.SetActive(false);
+            _mainMenu.SetMenuScreen(_hasSaveData);
+        }
+
+        public void ShowExitConfirmationPopup()
+        {
+            _popupPanel.ConfirmationResponseAction += HideExitConfirmationPopup;
+            _popupPanel.gameObject.SetActive(true);
+            _popupPanel.SetPopup(PopupType.Quit);
+        }
+        
+        void HideExitConfirmationPopup(bool quitConfirmed)
+        {
+            _popupPanel.ConfirmationResponseAction -= HideExitConfirmationPopup;
+            _popupPanel.gameObject.SetActive(false);
+            
+            if (quitConfirmed)
+            {
+                Application.Quit();
+            }
+            
+            _mainMenu.SetMenuScreen(_hasSaveData);
+        }
+
+        private void OnDestroy()
+        {
+            _popupPanel.ConfirmationResponseAction -= HideExitConfirmationPopup;
+            _popupPanel.ConfirmationResponseAction -= StartNewGamePopupResponse;
+        }
+    }
+}
