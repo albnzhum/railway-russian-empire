553156fce1ab238792426b450c609dd63751c215
albnzhum <albnzhum@gmail.com>
2024-05-13 10:34:52 +0500

    audio manager

---
+++ Assets/Code/Audio/AudioCueEventChannelSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Railway.Audio
+{
+    [CreateAssetMenu(fileName = "New Audio Cue Event", menuName = "Events/Audio Cue Event")]
+    public class AudioCueEventChannelSO : ScriptableObject
+    {
+        
+    }
+}
--- Assets/Code/Audio/AudioManager.cs
+++ Assets/Code/Audio/AudioManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+namespace Railway.Audio
 {
+    public class AudioManager : MonoBehaviour
+    {
+    }
 }
